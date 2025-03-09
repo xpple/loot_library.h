803d84147a769b12657e64d7a0bb5ee042194c1b
Kludwisz <kludwisz@gmail.com>
2025-03-09 13:38:31 +0100

    add shipwreck supply chest for testing

--- src/examples/example_v2.c
+++ src/examples/example_v2.c
@@ -3,29 +3,33 @@
 #include "../v2/mc_loot.h"
 
 
+static void print_loot(LootTableContext* ctx)
+{
+	for (int i = 0; i < ctx->generated_item_count; i++)
+	{
+		ItemStack* item_stack = &(ctx->generated_items[i]);
+		printf("%s x %d\n", get_item_name(ctx, item_stack->item), item_stack->count);
+
+		for (int j = 0; j < item_stack->enchantment_count; j++)
+		{
+			EnchantInstance* ench = &(item_stack->enchantments[j]);
+			printf("    %s %d\n", get_enchantment_name(ench->enchantment), ench->level);
+		}
+	}
+}
+
 int main() {
 	//test_enchant_vec();
 	//return 0;
 
 	LootTableContext ctx;
-	init_loot_table("src/examples/end_city_treasure.json", &ctx, (MCVersion)v1_21);
-
+	//init_loot_table("src/examples/end_city_treasure.json", &ctx, (MCVersion)v1_21);
+	init_loot_table("src/examples/shipwreck_supply.json", &ctx, (MCVersion)v1_21);
 	set_loot_seed(&ctx, 123456ULL);
 	generate_loot(&ctx);
-
-	for (int i = 0; i < ctx.generated_item_count; i++)
-	{
-		ItemStack* item_stack = &ctx.generated_items[i];
-		printf("%s x %d\n", get_item_name(&ctx, item_stack->item), item_stack->count);
-
-		for (int j = 0; j < item_stack->enchantment_count; j++)
-		{
-			EnchantInstance* ench = &item_stack->enchantments[j];
-			printf("    %s %d\n", get_enchantment_name(ench->enchantment), ench->level);
-		}
-	}
-
+	print_loot(&ctx);
 	free_loot_table(&ctx);
 
 	return 0;
 }
+
